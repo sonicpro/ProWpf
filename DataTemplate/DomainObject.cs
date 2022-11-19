422d6a87d290ae6538debe107148232301c8176d
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2022-11-19 19:47:47 +0200

    DataTemplate demo.

---
+++ DataTemplate/DomainObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DataTemplate
+{
+    public class DomainObject
+    {
+        private int instanceId;
+
+        private static int instanceCounter;
+
+        public string DisplayText
+        {
+            get
+            {
+                var className = this.GetType().Name;
+                return $"{CamelCaseToHumanReadable(className)} #{instanceId}";
+            }
+        }
+
+        public DomainObject()
+        {
+            System.Threading.Interlocked.Increment(ref instanceCounter);
+            instanceId = instanceCounter;
+        }
+
+        private static string CamelCaseToHumanReadable(string camelCase)
+        {
+            var camelCaseMatches = Regex.Matches (camelCase, @"[a-z0-9][A-Z]").Cast<Match>();
+            var humanReadable = camelCaseMatches.Aggregate(camelCase, (current, match) => {
+                return current.Insert(match.Index + 1, " ");
+            });
+            return humanReadable;
+        }
+    }
+}
