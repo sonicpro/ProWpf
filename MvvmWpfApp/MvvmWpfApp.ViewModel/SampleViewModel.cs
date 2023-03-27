a007df1b912997efe7fa70da0a3d27f8f4dd7e5a
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2023-03-27 11:41:38 +0300

    Sample ViewModel demo from Chapter 4. ViewModel has not completed.

---
+++ MvvmWpfApp/MvvmWpfApp.ViewModel/SampleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MvvmWpfAppApp.Model;
+
+namespace MvvmWpfApp.ViewModel
+{
+    public class SampleViewModel
+    {
+        public double Result { get; private set; }
+
+        public double Number { get; set; }
+
+        public void CalculateSquareRootCommand()
+        {
+            var model = new SampleModel();
+            Result = model.CalculateSquareRoot(Number);
+        }
+    }
+}
---
+++ MvvmWpfApp/MvvmWpfAppApp.Model/SampleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvvmWpfAppApp.Model
+{
+    public class SampleModel
+    {
+        public double CalculateSquareRoot(double number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+}
