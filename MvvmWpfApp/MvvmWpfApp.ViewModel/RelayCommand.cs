dfd6662228e4683609ed66331bf0ed3b8ac10eaf
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2023-04-17 22:32:06 +0300

    Implemented INotifyPropertyChanged.

---
+++ MvvmWpfApp/MvvmWpfApp.ViewModel/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MvvmWpfApp.ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentException(nameof(execute));
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        #region ICommand implementation
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #endregion
+    }
+}
--- MvvmWpfApp/MvvmWpfApp.ViewModel/SampleViewModel.cs
+++ MvvmWpfApp/MvvmWpfApp.ViewModel/SampleViewModel.cs
@@ -1,17 +1,65 @@
-﻿using MvvmWpfAppApp.Model;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using MvvmWpfAppApp.Model;
 
 namespace MvvmWpfApp.ViewModel
 {
-    public class SampleViewModel
+    public class SampleViewModel : INotifyPropertyChanged
     {
-        public double Result { get; private set; }
+        private readonly SampleModel model;
+        private RelayCommand calculateSquareRootCommand;
+        private double result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SampleViewModel()
+        {
+            model = new SampleModel();
+        }
+
+        public double Result
+        {
+            // ReSharper disable once ArrangeAccessorOwnerBody
+            get { return result; }
+            private set
+            {
+                if (Math.Abs(result - value) < 0.0001)
+                {
+                    return;
+                }
+
+                result = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+        }
 
         public double Number { get; set; }
 
-        public void CalculateSquareRootCommand()
+        public ICommand CalculateSquareRootCommand
         {
-            var model = new SampleModel();
+            get
+            {
+                if (calculateSquareRootCommand == null)
+                {
+                    calculateSquareRootCommand = new RelayCommand(parameter =>
+                        CalculateSquareRoot());
+                }
+
+                return calculateSquareRootCommand;
+            }
+        }
+
+        #region Methods
+
+        private void CalculateSquareRoot()
+        {
             Result = model.CalculateSquareRoot(Number);
         }
+
+        #endregion
     }
 }
