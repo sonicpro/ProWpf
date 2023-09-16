6410a7de7352bb1885fd25f81231f52134f876df
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2023-09-16 16:49:41 +0300

    Chapter 5 - RelayCommand demo draft.

---
+++ RelayCommand/ViewModel/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _executeHandler;
+        private readonly Predicate<object> _canExecuteHandler;
+
+        public RelayCommand(Action<object> executeHandler)
+        {
+            _executeHandler = executeHandler;
+        }
+
+        public RelayCommand(Action<object> executeHandler, Predicate<object> canExecuteHandler) : this(executeHandler)
+        {
+            _canExecuteHandler = canExecuteHandler;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteHandler == null || _canExecuteHandler(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeHandler(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
